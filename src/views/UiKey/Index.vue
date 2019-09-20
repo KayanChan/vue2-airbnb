6c011f6401d32de842a368d4ccc905b3dd61c6dc
KayanChan <1010136337@qq.com>
2019-09-20 17:56:25 +0800

    CSS Module尝试

--- src/views/UiKey/Index.vue
+++ src/views/UiKey/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    初始化构建完成
+    <h3>Button</h3>
+    <button :class="$style.button">xxx</button>
   </div>
 </template>
 <script>
@@ -11,3 +12,29 @@ export default {
   }
 }
 </script>
+<style module lang="less">
+  @svg 1px-border {
+    width: 4px;
+    height: 4px;
+    @rect {
+      fill: transparent;  /* content为透明 */
+      width: 100%;  /* 宽度为4px * 100% */
+      height: 100%;  /* 高度为4px * 100% */
+      stroke-width: 25%;  /* 边框宽度 4px * 25%(即1px) */
+      stroke: var(--color, black);  /* 颜色 */
+    }
+  }
+
+  #real-1px {
+    height: 50px;
+  }
+
+  #real-1px {
+    border: 1px solid;
+    border-image: svg(1px-border param(--color red)) 1 stretch;
+  }
+  :global(.button) {
+    padding: 6px 12px;
+    line-height: 20px;
+  }
+</style>
